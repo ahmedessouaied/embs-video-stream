713dd911c62ff2c5f86b666747b1f3559ef21cf8
ahmedessouaied <ahmed.essouaied@supcom.tn>
2024-12-16 14:01:19 +0100

    ee

--- src/app/meeting/[id]/left/page.tsx
+++ src/app/meeting/[id]/left/page.tsx
@@ -2,11 +2,12 @@ import { buttonClassName } from "@/components/Button"
 import { cn } from "@/lib/utils"
 import Link from "next/link"
 
-// interface PageProps {
-//     params: { id: string }
-// }
+ interface PageProps {
+     params: { id: string }
+ }
 
-export default function Page({ params: { id } }) {
+export default function Page({ params }: PageProps ) {
+    const { id } = params;
     return <div className="flex flex-col items-center gap3">
         <p className="fontbold">You left this meeting.</p>
         <Link
